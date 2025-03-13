0674852a71d2950daabf2574cfb6543ff3557b70
CamiloDuranMesa <duranmesacamilo@gmail.com>
2025-03-13 12:54:41 -0500

    Imagen de home ajustada

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,6 +10,8 @@ export const Home = () => {
         title="Bienvenidos a mi portafolio, Soy Camilo Durán Mesa"
         text="Soy un programador dedicado con experiencia en Java, JavaScript, C#, SQL (MySQL) y MongoDB, especializado en desarrollo de software, gestión de bases de datos y microservicios. Tengo habilidades en arquitectura de sistemas, optimización de código y creación de soluciones eficientes. Me enfoco en escribir código limpio y estructurado, priorizando la claridad y el rendimiento."
         image={Image}
+        imageWidth="w-[20rem]"  
+        imageHeight="h-[25rem]"
       />
     </Main>
   )
